02118f72efd5e0bc7522c992a39d3c2fd5a83d28
Travis CI User <travis@example.org>
2018-04-18 12:58:06 +0000

    Automated: Updating code examples

--- .repos/nexmo-community/nexmo-ruby-quickstart/initialize/basic.rb
+++ .repos/nexmo-community/nexmo-ruby-quickstart/initialize/basic.rb
@@ -1,6 +1,6 @@
 require 'nexmo'
 
 client = Nexmo::Client.new(
-  api_key: NEXMO_API_KEY,
-  api_secret: NEXMO_API_SECRET
+  key: NEXMO_API_KEY,
+  secret: NEXMO_API_SECRET
 )
--- .repos/nexmo-community/nexmo-ruby-quickstart/voice/outbound_tts_call.rb
+++ .repos/nexmo-community/nexmo-ruby-quickstart/voice/outbound_tts_call.rb
@@ -12,13 +12,13 @@ TO_NUMBER = ENV['TO_NUMBER']
 require 'nexmo'
 
 client = Nexmo::Client.new(
-  api_key: NEXMO_API_KEY,
-  api_secret: NEXMO_API_SECRET,
+  key: NEXMO_API_KEY,
+  secret: NEXMO_API_SECRET,
   application_id: NEXMO_APPLICATION_ID,
   private_key: File.read(NEXMO_APPLICATION_PRIVATE_KEY_PATH)
 )
 
-client.calls.create(
+client.create_call(
   to: [{
     type: 'phone',
     number: TO_NUMBER
