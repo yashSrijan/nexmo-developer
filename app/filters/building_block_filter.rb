411189f0546eed508253c975a7c938a388b00ea7
Michael Heap <m@michaelheap.com>
2018-05-26 15:03:18 +0200

    Refactor building block filter to remove duplication

--- app/filters/building_block_filter.rb
+++ app/filters/building_block_filter.rb
@@ -3,50 +3,32 @@ class BuildingBlockFilter < Banzai::Filter
     input.gsub(/```single_building_block(.+?)```/m) do |_s|
       config = YAML.safe_load($1)
 
-      if config['config']
-        configs = YAML.load_file("#{Rails.root}/config/code_examples.yml")
-        config = config['config'].split('.').inject(configs) { |h, k| h[k] }
+      lexer = language_to_lexer(config['language'])
+
+      # Read the client
+      if config['client']
+          highlighted_client_source = generate_code_block(config['client'], config['unindent'])
       end
 
-      code = File.read("#{Rails.root}/#{config['source']}")
-      language = File.extname("#{Rails.root}/#{config['source']}")[1..-1]
-      lexer = language_to_lexer(language)
-
-      total_lines = code.lines.count
-
-      # Minus one since lines are not zero-indexed
-      from_line = (config['from_line'] || 1) - 1
-      to_line = (config['to_line'] || total_lines) - 1
-
-      code = code.lines[from_line..to_line].join
-      code.unindent! if config['unindent']
-
-      highlighted_source = highlight(code, lexer)
-
-      line_numbers = (1..total_lines).map do |line_number|
-        <<~HEREDOC
-          <span class="focus__lines__line">#{line_number}</span>
-        HEREDOC
-      end
-
-      dep_managers = {
-        "javascript" => "npm install",
-        "php" => "composer require"
-      }
+      # Read the code
+      highlighted_code_source = generate_code_block(config['code'], config['unindent'])
 
       dependency_html = ''
       if config['dependencies']
-          dependencies = config['dependencies'].join(" ")
+          dependency_html = generate_dependencies(lexer.tag, config['dependencies'])
+      end
 
-          dependency_html = <<~HEREDOC
-          <p>Install the required dependencies:</p>
-          <pre class="highlight bash"><code>$ #{dep_managers[lexer.tag]} #{dependencies}</code></pre>
-          HEREDOC
+      client_html = ""
+      if highlighted_client_source
+      client_html = <<~HEREDOC
+        <p>Create a file named <code>#{config['file_name']}</code> and initialize the client:</p>
+        <pre class="highlight #{lexer.tag}"><code>#{highlighted_client_source}</code></pre>
+      HEREDOC
       end
 
       code_html = <<~HEREDOC
-        <p>Create <code>#{config['file_name']}</code> with the following contents:</p>
-        <pre class="highlight #{lexer.tag}"><code>#{highlighted_source}</code></pre>
+        <p>Add the following to <code>#{config['file_name']}</code>:</p>
+        <pre class="highlight #{lexer.tag}"><code>#{highlighted_code_source}</code></pre>
       HEREDOC
 
       run_html = <<~HEREDOC
@@ -58,7 +40,7 @@ class BuildingBlockFilter < Banzai::Filter
 
       HEREDOC
 
-      dependency_html + code_html + run_html
+      dependency_html + client_html + code_html + run_html
     end
   end
 
@@ -85,5 +67,44 @@ class BuildingBlockFilter < Banzai::Filter
 
   def language_configuration
     @language_configuration ||= YAML.load_file("#{Rails.root}/config/code_languages.yml")
+  end
+
+
+  def generate_code_block(input, unindent)
+      # TODO: Remove the n/a check by updating YAML definitions
+      if input && input['source'] != 'n/a'
+          code = File.read("#{Rails.root}/#{input['source']}")
+          language = File.extname("#{Rails.root}/#{input['source']}")[1..-1]
+          lexer = language_to_lexer(language)
+
+          total_lines = code.lines.count
+
+          # Minus one since lines are not zero-indexed
+          from_line = (input['from_line'] || 1) - 1
+          to_line = (input['to_line'] || total_lines) - 1
+
+          code = code.lines[from_line..to_line].join
+          code.unindent! if unindent
+
+          highlight(code, lexer)
+      end
+  end
+
+  def generate_dependencies(language, dependencies)
+      dep_managers = {
+          "javascript" => lambda { |deps| "npm install #{dependencies.join(' ')}" },
+          "php" => lambda { |deps| "composer require #{dependencies.join(' ')}" },
+          "java" => lambda { |deps| <<~HEREDOC
+            Add the following to <code>build.gradle</code>
+            #{dependencies.map { |d| "compile '#{d}'<br />" }}
+          HEREDOC
+          },
+      }
+
+      deps = dep_managers[language].call(dependencies)
+      dependency_html = <<~HEREDOC
+          <p>Install the required dependencies:</p>
+          <pre class="highlight bash"><code>$ #{deps}</code></pre>
+      HEREDOC
   end
 end
