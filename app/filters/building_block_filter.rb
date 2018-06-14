d40fbdbc2594f442e49877ef15ae011178b18824
Michael Heap <m@michaelheap.com>
2018-06-14 17:01:02 +0100

    Regenerate building blocks

--- app/filters/building_block_filter.rb
+++ app/filters/building_block_filter.rb
@@ -31,14 +31,7 @@ class BuildingBlockFilter < Banzai::Filter
         <pre class="highlight #{lexer.tag}"><code>#{highlighted_code_source}</code></pre>
       HEREDOC
 
-      run_html = <<~HEREDOC
-        ## Run your code
-
-        Save this file to your machine and run it:
-
-        <pre class="highlight bash"><code>$ #{config['run_command']}</code></pre>
-
-      HEREDOC
+      run_html = generate_run_command(config['run_command'], config['file_name'])
 
       dependency_html + client_html + code_html + run_html
     end
@@ -71,9 +64,12 @@ class BuildingBlockFilter < Banzai::Filter
 
   def generate_code_block(input, unindent)
       # TODO: Remove the n/a check by updating YAML definitions
+      filename = "#{Rails.root}/#{input['source']}"
       if input && input['source'] != 'n/a'
-          code = File.read("#{Rails.root}/#{input['source']}")
-          language = File.extname("#{Rails.root}/#{input['source']}")[1..-1]
+          raise "BuildingBlockFilter - Could not load #{filename}" unless File.exist?(filename)
+
+          code = File.read(filename)
+          language = File.extname(filename)[1..-1]
           lexer = language_to_lexer(language)
 
           total_lines = code.lines.count
@@ -82,6 +78,7 @@ class BuildingBlockFilter < Banzai::Filter
           from_line = (input['from_line'] || 1) - 1
           to_line = (input['to_line'] || total_lines) - 1
 
+          puts code
           code = code.lines[from_line..to_line].join
           code.unindent! if unindent
 
@@ -112,5 +109,38 @@ class BuildingBlockFilter < Banzai::Filter
           <p>#{deps['text']}</p>
           <pre class="highlight bash"><code>#{deps['code']}</code></pre>
       HEREDOC
+  end
+
+  def generate_run_command(command, filename)
+      if command == 'java-ide'
+          command = <<~HEREDOC
+## Run your code
+We can use the `application` plugin for Gradle to simplify the running of our application.
+
+Update your `build.gradle` with the following:
+
+```groovy
+apply plugin: 'application'
+mainClassName = project.hasProperty('main') ? project.getProperty('main') : ''
+```
+
+Run the following command to execute your application:
+
+```sh
+gradle run -Pmain=com.nexmo.quickstart.voice.#{filename.gsub(".java","")}
+```
+HEREDOC
+      else
+      command = <<~HEREDOC
+        ## Run your code
+
+        Save this file to your machine and run it:
+
+        <pre class="highlight bash"><code>$ #{command}</code></pre>
+
+      HEREDOC
+      end
+
+      command
   end
 end
